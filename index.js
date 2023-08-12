3f72773b5e5a20c7553b332047f38d87dab0de76
Anchal <iamdanchal@gmail.com>
2023-08-12 15:54:08 +0530

    calculator

--- index.js
+++ index.js
@@ -1,71 +1,34 @@
 const express=require("express");
-require('./config');
-const Product=require('./products');
 const app=express();
-const multer=require("multer");//upload files in a folder  
-
-
-const upload=multer({
-    storage:multer.diskStorage({
-        destination:function(req,file,cb)
-        {
-            cb(null,"upload")
-        },
-        filename:function(req,file,cb)
-        {
-            cb(null,file.fieldname+"-"+Date.now()+ ".jpg");
-        }
-    })
-}).single("userfile")
-
-
-app.post("/upload",upload,(req,res)=>{
-    res.send("file uploaded");
-})
 
 app.use(express.json());
-app.post("/create",async (req, res)=>{
 
-    let data=new Product(req.body);
-    let result=await data.save();
+app.post("/calc",(req,res)=>{
     console.log(req.body);
-    res.send(result);
-});
+    let a = parseInt(req.body.a);
+    let b = parseInt(req.body.b);
+    let op=req.body.op;
+    console.log(req.body.op);
+    switch(op) {
+        case "+":
+            c=a+b;
+          break;
 
-app.get("/listen",async(req,res)=>{
-    let data=await Product.find();
-    res.send(data);
+        case "-":
+          c=a-b;
+          break;
+          
+        case "*":
+          c=a*b;
+          break;
+        case "/":
+          c=a/b;
+          break;
+        default:
+          c="Invalid"
+      }
+     
+      res.send(JSON.stringify(c));
 })
 
-app.delete("/delete/:_id",async (req,res)=>{
-    console.log(req.params);
-    let data=await Product.deleteOne(req.params);
-    res.send(data);
-})
-
-app.put("/update/:_id",async (req,res)=>{
-    console.log(req.params);
-    let data=await Product.updateOne(
-        req.params,
-        {
-            $set:req.body
-        }
-        );
-    res.send(data);
-})
-
-//get uaing more then 1 field
-app.get("/search/:key",async(req, res)=>{
-    console.log(req.params.key)
-    let data=await Product.find(
-        {
-            "$or":[
-                {"name":{$regex:req.params.key}},
-                {"brand":{$regex:req.params.key}},
-            ]
-        }
-    )
-    res.send(data);
-    console.log(data);
-})
-app.listen(4000);
+app.listen(5000);
--- products.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const ProductSch = new mongoose.Schema({
-    name: String,
-    price: Number,
-    brand: String,
-    category: String
-});
-module.exports=mongoose.model('products',ProductSch);
