3183fdccf227ac3e96d4fbe021afe7c79e1cafab
Anchal <iamdanchal@gmail.com>
2023-08-12 14:51:34 +0530

    api using mongoose ,upload file in folder,get using more then 1 fields

--- index.js
+++ index.js
@@ -1,49 +1,71 @@
-const mongoose = require("mongoose");
+const express=require("express");
+require('./config');
+const Product=require('./products');
+const app=express();
+const multer=require("multer");//upload files in a folder  
 
-mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
-const ProductSch = new mongoose.Schema({
-    name: String,
-    price: Number,
-    brand: String,
-    category: String
+
+const upload=multer({
+    storage:multer.diskStorage({
+        destination:function(req,file,cb)
+        {
+            cb(null,"upload")
+        },
+        filename:function(req,file,cb)
+        {
+            cb(null,file.fieldname+"-"+Date.now()+ ".jpg");
+        }
+    })
+}).single("userfile")
+
+
+app.post("/upload",upload,(req,res)=>{
+    res.send("file uploaded");
+})
+
+app.use(express.json());
+app.post("/create",async (req, res)=>{
+
+    let data=new Product(req.body);
+    let result=await data.save();
+    console.log(req.body);
+    res.send(result);
 });
-const ProductMod = mongoose.model('products', ProductSch);
-const InsertInDB = async () => {
-    
-    let data = new ProductMod(
-        {
-            name: "abc",
-            price: 10,
-            brand: "xyz",
-            category: "fgh"
-        });
-    let result = await data.save();
-    console.log(result);
-}
 
-const UpdateInDB = async () => {
-    const ProductMod = mongoose.model('products', ProductSch);
-    let data = await ProductMod.updateOne(
-        { name: "abc" },
+app.get("/listen",async(req,res)=>{
+    let data=await Product.find();
+    res.send(data);
+})
+
+app.delete("/delete/:_id",async (req,res)=>{
+    console.log(req.params);
+    let data=await Product.deleteOne(req.params);
+    res.send(data);
+})
+
+app.put("/update/:_id",async (req,res)=>{
+    console.log(req.params);
+    let data=await Product.updateOne(
+        req.params,
         {
-            $set: { price: 30 }
+            $set:req.body
+        }
+        );
+    res.send(data);
+})
+
+//get uaing more then 1 field
+app.get("/search/:key",async(req, res)=>{
+    console.log(req.params.key)
+    let data=await Product.find(
+        {
+            "$or":[
+                {"name":{$regex:req.params.key}},
+                {"brand":{$regex:req.params.key}},
+            ]
         }
     )
+    res.send(data);
     console.log(data);
-}
-
-const deleteInDB= async()=>{
-    const ProductMod = mongoose.model('products', ProductSch);
-    let data=await ProductMod.deleteOne({
-        price:10
-    })
-console.log(data);
-}
-
-const finInDB= async()=>{
-    const ProductMod = mongoose.model('products', ProductSch);
-    let data=await ProductMod.find();
-console.log(data);
-}
-
-finInDB();
+})
+app.listen(4000);
---
+++ products.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const ProductSch = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String
+});
+module.exports=mongoose.model('products',ProductSch);
